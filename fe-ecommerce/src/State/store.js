8d88578af82e1b4281b5877ebce21aaf0f1e36c6
TienTran1504 <74586033+TienTran1504@users.noreply.github.com>
2024-06-17 17:45:50 +0700

    feat: setup action, reducer for Cart, Order, Product

--- fe-ecommerce/src/State/store.js
+++ fe-ecommerce/src/State/store.js
@@ -1,9 +1,15 @@
 import { configureStore, combineReducers } from '@reduxjs/toolkit';
 import { thunk } from 'redux-thunk';
 import { authReducer } from './Auth/Reducer';
+import { productReducer } from './Product/Reducer';
+import { cartReducer } from './Cart/Reducer';
+import { orderReducer } from './Order/Reducer';
 
 const rootReducer = combineReducers({
     auth: authReducer,
+    product: productReducer,
+    cart: cartReducer,
+    order: orderReducer
 });
 
 export const store = configureStore({
